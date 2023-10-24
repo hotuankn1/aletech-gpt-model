91e724c455f3a00d52103898fd513097f2947cb9
hieu.ho <hotuankn1@gmail.com>
2023-10-25 00:01:36 +0700

    add createChat

--- service/IChatService.ts
+++ service/IChatService.ts
@@ -1,6 +1,7 @@
 import { IChat } from '../type/IChat';
 
 export interface IChatService {
+  createChat(userId: number): Promise<IChat>
   getAll(userId: number): Promise<IChat[]>;
   getById(chatId: number): Promise<IChat>;
 }
