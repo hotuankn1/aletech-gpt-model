5bcbf9764ac048911f84adc00ac4ea27abc89d5b
hieu.ho <hotuankn1@gmail.com>
2023-11-09 09:58:42 +0700

    change logic of word api

--- service/IWordService.ts
+++ service/IWordService.ts
@@ -3,8 +3,7 @@ import { IWord } from "../type/IWord";
 
 export interface IWordService {
     getList (page: number, pageSize: number): Promise<IPagination<IWord>>;
-    create(createdBy: number): Promise<IWord>;
-    update(word: IWord): Promise<IWord>;
+    createOrUpdate(word: IWord): Promise<IWord>;
     delete(id: number): Promise<void>;
     getById(id:number): Promise<IWord>;
 }
