1d2f9ff25a2ca32460e7a1e8f14083a666b2fe3e
hieu.ho <hotuankn1@gmail.com>
2023-11-09 19:45:02 +0700

    change logic intent service

--- service/IIntentService.ts
+++ service/IIntentService.ts
@@ -3,8 +3,7 @@ import { IPagination } from "../type/IPagination";
 
 export interface IIntentService {
     getList (page: number, pageSize: number): Promise<IPagination<IIntent>>;
-    create(createdBy: number): Promise<IIntent>;
-    update(intent: IIntent): Promise<IIntent>;
+    createOrUpdate(intent: IIntent): Promise<IIntent>;
     delete(id: number): Promise<void>;
     getById(id:number): Promise<IIntent>;
 }
