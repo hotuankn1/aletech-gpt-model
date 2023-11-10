899b3f7e4bedd4564523b7ea0d116ee35635df52
hieu.ho <hieu.ale.it@gmail.com>
2023-11-10 15:14:58 +0000

    change interface search

--- service/IIntentService.ts
+++ service/IIntentService.ts
@@ -1,9 +1,13 @@
-import { IIntent } from "../type/IIntent";
-import { IPagination } from "../type/IPagination";
+import { IIntent } from '../type/IIntent';
+import { IPagination } from '../type/IPagination';
 
 export interface IIntentService {
-    getList (page: number, pageSize: number): Promise<IPagination<IIntent>>;
-    createOrUpdate(intent: IIntent): Promise<IIntent>;
-    delete(id: number): Promise<void>;
-    getById(id:number): Promise<IIntent>;
+  getList(
+    page: number,
+    pageSize: number,
+    search: string,
+  ): Promise<IPagination<IIntent>>;
+  createOrUpdate(intent: IIntent): Promise<IIntent>;
+  delete(id: number): Promise<void>;
+  getById(id: number): Promise<IIntent>;
 }
