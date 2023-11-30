02e42cf2122aa9740805c0984c0cae1a4537dee6
hieu.ho <hotuankn1@gmail.com>
2023-11-30 13:28:19 +0700

    AG-85 [BE] Add column role including user and admin for User entity

--- service/IMessageService.ts
+++ service/IMessageService.ts
@@ -1,6 +1,7 @@
+import { User } from 'src/user/user.entity';
 import { IMessage } from '../type/IMessage';
 
 export interface IMessageService {
-  response(content: string, chatId: number): Promise<IMessage>;
+  response(content: string, chatId: number, user: User): Promise<IMessage>;
   get1000LatestMessages(chatId: number): Promise<IMessage[]>;
 }
