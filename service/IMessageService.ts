c47ca6ea7f77733b4fd3da0a2db615fb49a2d879
hieu.ho <hotuankn1@gmail.com>
2023-11-30 18:00:19 +0700

    change User to IUser

--- service/IMessageService.ts
+++ service/IMessageService.ts
@@ -1,7 +1,7 @@
-import { User } from 'src/user/user.entity';
 import { IMessage } from '../type/IMessage';
+import { IUser } from '../type/IUser';
 
 export interface IMessageService {
-  response(content: string, chatId: number, user: User): Promise<IMessage>;
+  response(content: string, chatId: number, user: IUser): Promise<IMessage>;
   get1000LatestMessages(chatId: number): Promise<IMessage[]>;
 }
