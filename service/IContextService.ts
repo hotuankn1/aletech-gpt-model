1b20e2590a8be33193a00922d7d41229b9eae461
hieu.ho <hotuankn1@gmail.com>
2023-12-02 17:55:52 +0700

    AG-90 [BE] Define interface for Context

---
+++ service/IContextService.ts
@@ -0,0 +1,10 @@
+import { IContext } from "../type/IContext";
+import { IPagination } from "../type/IPagination";
+
+export interface IContextService {
+    getList(
+        page: number,
+        pageSize: number,
+    ): Promise<IPagination<IContext>>;
+    createOrUpdate(context: IContext): Promise<IContext>;
+}
