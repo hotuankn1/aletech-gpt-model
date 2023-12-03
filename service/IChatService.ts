0c70ec476f78e834dcb3f5bf8319023fd03999b8
hieu.ho <hotuankn1@gmail.com>
2023-12-03 10:39:00 +0700

    AG-75 [BE] Add column deletedAt for Chat

--- service/IChatService.ts
+++ service/IChatService.ts
@@ -4,5 +4,6 @@ export interface IChatService {
   createChat(userId: number, name:string): Promise<IChat>
   getAll(userId: number): Promise<IChat[]>;
   getById(chatId: number): Promise<IChat>;
-  update(chat: IChat): Promise<IChat>
+  update(chat: IChat): Promise<IChat>;
+  delete(id: number): Promise<IChat>;
 }
